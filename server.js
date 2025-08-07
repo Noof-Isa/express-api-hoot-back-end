34110c7cf9d9aa1de0ddd4b61d63a55f25ee7d9a
Noof-Isa <noofisa786@gmail.com>
2025-08-07 10:34:21 +0300

    create route done

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 
-const app = express();
-const mongoose = require('mongoose');
-const cors = require('cors');
-const logger = require('morgan');
+const app = express()
+const mongoose = require('mongoose')
+const cors = require('cors')
+const logger = require('morgan')
 
 // Controllers
 const testJwtRouter = require('./controllers/test-jwt');
 const authRouter = require('./controllers/auth');
 const userRouter = require('./controllers/users');
+const hootsRouter = require('./controllers/hoots.js');
 
 // Middleware
 const verifyToken = require('./middleware/verify-token');
@@ -34,8 +35,8 @@ app.use('/auth', authRouter);
 app.use('/test-jwt', testJwtRouter);
 
 // PROTECTED ROUTES
-app.use(verifyToken);
 app.use('/users', userRouter);
+app.use('/hoots', hootsRouter);
 
 app.listen(3000, () => {
   console.log('The express app is ready!');
