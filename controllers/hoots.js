f939c98dafd88eee2df65dab63d43fe7f83a41bc
Noof-Isa <noofisa786@gmail.com>
2025-08-07 11:09:04 +0300

    show,update and delete

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -6,6 +6,28 @@ const router = express.Router()
 
 // ========== Public Routes ===========
 
+//GET ALL THE HOOTS
+router.get('/', async (req, res) => {
+	try {
+		const hoots = await Hoot.find()
+			.populate('author')
+			.sort({ createdAt: 'desc' })
+		res.status(200).json(hoots)
+	} catch (error) {
+		res.status(500).json(error)
+	}
+})
+
+// GET A SINGLE HOOT (SHOW)
+router.get('/:hootId', async (req, res) => {
+	try {
+		const hoot = await Hoot.findById(req.params.hootId).populate('author')
+		res.status(200).json(hoot)
+	} catch (error) {
+		res.status(500).json(error)
+	}
+})
+
 // ========= Protected Routes =========
 
 router.use(verifyToken)
@@ -19,6 +41,50 @@ router.post('/', async (req, res) => {
 		res.status(201).json(hoot)
 	} catch (error) {
 		console.log(error)
+		res.status(500).json(error)
+	}
+})
+
+// UPDATE A SINGLE HOOT
+router.put('/:hootId', async (req, res) => {
+	try {
+		// Find the hoot:
+		const hoot = await Hoot.findById(req.params.hootId)
+
+		// Check permissions:
+		if (!hoot.author.equals(req.user._id)) {
+			return res.status(403).send("You're not allowed to do that!")
+		}
+
+		// Update hoot:
+		const updatedHoot = await Hoot.findByIdAndUpdate(
+			req.params.hootId,
+			req.body,
+			{ new: true }
+		)
+
+		// Append req.user to the author property:
+		updatedHoot._doc.author = req.user
+
+		// Issue JSON response:
+		res.status(200).json(updatedHoot)
+	} catch (error) {
+		res.status(500).json(error)
+	}
+})
+
+// DELETE A HOOT
+router.delete('/:hootId', async (req, res) => {
+	try {
+		const hoot = await Hoot.findById(req.params.hootId)
+
+		if (!hoot.author.equals(req.user._id)) {
+			return res.status(403).send("You're not allowed to do that!")
+		}
+
+		const deletedHoot = await Hoot.findByIdAndDelete(req.params.hootId)
+		res.status(200).json(deletedHoot)
+	} catch (error) {
 		res.status(500).json(error)
 	}
 })
