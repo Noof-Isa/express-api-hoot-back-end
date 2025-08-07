e26bec33a35acd594cf53ee26d7fb01039ec937a
Noof-Isa <noofisa786@gmail.com>
2025-08-07 10:09:27 +0300

    create a model

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,51 @@
+// // controllers/users.js
+
+// const express = require('express');
+
+// const router = express.Router();
+// const User = require('../models/user');
+// const verifyToken = require('../middleware/verify-token')
+
+
+
+
+// router.get('/currentUser', async (req, res) => {
+//   try {
+//     const user = await User.findById(req.user._id);
+//     if (!user) {
+//       return res.status(404).json({ err: 'User not found.' });
+//     }
+
+//     res.json({ user });
+//   } catch (err) {
+//     res.status(500).json({ err: err.message });
+//   }
+// });
+
+// router.get('/:userId', verifyToken, async (req, res) => {
+//   try {
+//     // If the user is looking for the details of another user, block the request
+//     // Send a 403 status code to indicate that the user is unauthorized
+//     if (req.user._id !== req.params.userId) {
+//       return res.status(403).json({ err: 'Unauthorized' });
+//     }
+
+//     const user = await User.findById(req.params.userId);
+
+//     if (!user) {
+//       return res.status(404).json({ err: 'User not found.' });
+//     }
+
+//     res.json({ user });
+//   } catch (err) {
+//     res.status(500).json({ err: err.message });
+//   }
+// });
+
+// module.exports = router;
+
+// models/user.js
+
 // controllers/users.js
 
 const express = require('express');
@@ -5,9 +53,6 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const verifyToken = require('../middleware/verify-token')
-
-
-
 
 router.get('/currentUser', async (req, res) => {
   try {
